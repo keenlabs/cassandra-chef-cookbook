3ec80b6874c43cbc6c3d327dfe09f94e43eb9e77
Daniel Kador <dkador@gmail.com>
2013-06-30 22:31:19 -0700

    use the template

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,6 @@
 include_recipe "cassandra::datastax"
 
-%w(cassandra.yaml cassandra-env.sh).each do |f|
+%w(cassandra.yaml cassandra-env.sh cassandra-topology.properties).each do |f|
   template File.join(node["cassandra"]["conf_dir"], f) do
     source "#{f}.erb"
     owner node["cassandra"]["user"]
